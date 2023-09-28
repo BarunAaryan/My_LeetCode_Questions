a8524180de16238ac7d9da516a4000290e6253e2
Barun Aaryan <93857428+BarunAaryan@users.noreply.github.com>
2023-09-28 13:22:52 +0530

    Time: 1 ms (100%), Space: 45 MB (53.52%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        //Start with first position
+        int insertPosition =0;
+        for(int i=0; i<nums.length; i++){
+//Fill all non zeroes
+if(nums[i]!=0){
+    nums[insertPosition]=nums[i];
+    insertPosition++;
+}
+        }
+while(insertPosition<nums.length){
+    nums[insertPosition++]=0;
+}
+        }
+
+    }
