17ae2208fa5b3e5d9dc4f7f7d825335659cc6d60
Barun Aaryan <93857428+BarunAaryan@users.noreply.github.com>
2024-10-15 13:18:15 +0530

    Time: 0 ms (100%), Space: 42.7 MB (53.48%) - LeetHub

---
+++ 1552-build-an-array-with-stack-operations/1552-build-an-array-with-stack-operations.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<String> buildArray(int[] target, int n) {
+        List<String> ans = new ArrayList<>();
+        int targetPointer =0, streamPointer=1;
+        while(targetPointer < target.length){
+        ans.add("Push");
+        if(target[targetPointer] == streamPointer){
+            targetPointer++;
+        }else{
+            ans.add("Pop");
+        }
+        streamPointer++;
+        }
+        return ans;
+        
+    }
+}
