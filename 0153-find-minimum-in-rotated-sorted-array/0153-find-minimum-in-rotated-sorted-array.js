797b15544d2ca7a58fd6a266e5cc601eb6f31641
Barun Aaryan <93857428+BarunAaryan@users.noreply.github.com>
2026-03-21 22:24:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    let low=0, high =nums.length -1;
+    while(low<high){
+        let mid= Math.floor(low+(high-low)/2);
+        if(nums[mid]>nums[high]){
+            //Min lies in right half
+            low= mid +1;
+        }else{
+            high=mid;
+        }
+    }
+    //return min element
+    return nums[low];
+    
+};
