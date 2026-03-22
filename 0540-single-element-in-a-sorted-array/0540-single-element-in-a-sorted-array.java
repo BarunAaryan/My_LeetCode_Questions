095fdd13c27b622628dc01ec1d4c2b474ae7d614
Barun Aaryan <93857428+BarunAaryan@users.noreply.github.com>
2026-03-22 22:46:35 +0530

    Time: 0 ms (100%), Space: 53.1 MB (31.57%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,35 +1,36 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int n= nums.length; //size of array
-        //edge cases 
-        if(n==1){
-            return nums[0];
-        }
-        if(nums[0]!=nums[1]){
-            return nums[0];
-        }
-        if(nums[n-1]!= nums[n-2]){
-            return nums[n-1];
-        }
-        int low =1, high = n-2;
-        while(low<=high){
-            int mid=(low+high)/2;
+        //Get the size of the array
+        int n =nums.length;
+        //Edge Case: Only one element in the array
+        if(n==1) return nums[0];
+        //Edge Case: First Element is the unique one
+        if (nums[0] != nums[1]) return nums[0];
+        //Edge Case: Last Element is the unique one
+        if (nums[n-1]!= nums[n-2]) return nums[n-1];
 
-            //if nums[mid] is single element
-            if(nums[mid]!= nums[mid+1] && nums[mid]!= nums[mid-1]){
+        int low=1, high = n-2;
+        //Perform Binary Search
+        while(low<=high){
+            int mid =  low+(high-low)/2;
+            //Check if middle element is the unique one
+            if(nums[mid]!= nums[mid-1] && nums[mid]!=nums[mid+1]){
                 return nums[mid];
             }
-            //we are in left
-            if((mid %2==1 && nums[mid]== nums[mid-1]) || (mid %2==0 && nums[mid]== nums[mid+1])){
-//eliminate the left half
-low= mid+1;
+            if ((mid % 2 == 1 && nums[mid] == nums[mid - 1]) || //odd
+                (mid % 2 == 0 && nums[mid] == nums[mid + 1])) {
+                // Move to the right half
+                low = mid + 1;
             }
-            //we are in the right
-            else{
-                //eliminate the right
-                high= mid-1;
+            // If mid is in the right half (pairing broken earlier)
+            else {
+                // Move to the left half
+                high = mid - 1;
             }
         }
-return -1;
+
+        // Dummy return (not reachable if input is valid)
+        return -1;
+        }
+        
     }
-}
