ebcb49f8fdde007f027696b2ff9040dfc61c772c
Barun Aaryan <93857428+BarunAaryan@users.noreply.github.com>
2026-06-06 22:11:57 +0530

    Time: 0 ms (100%), Space: 44.1 MB (23.12%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,27 +1,23 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        //base case 
-        //if matrix empty
-        if (matrix.length == 0 || matrix[0].length == 0) {
-            return false;
+         int n = matrix.length;
+        // Get the number of columns
+        int m = matrix[0].length;
+        int low = 0;
+        int high = n * m - 1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            // Map 1D index to 2D coordinates
+            int row = mid/m;
+            int col= mid%m;
+            if(matrix[row][col]==target){
+                return true;
+            }else if(matrix[row][col]<target){
+                low= mid+1;
+            }else{
+                high = mid-1;
+            }
         }
-        int n = matrix.length;
-        int m= matrix[0].length;
-        int lo=0;
-        int hi= (n*m)-1;
-
-while(lo<=hi){
-    int mid=(lo + (hi-lo)/2);
-    if(matrix[mid/m][mid%m]==target){
-        return true;
-    }
-    //search in right half
-    if(matrix[mid/m][mid%m]<target){
-        lo=mid+1;
-    }else{
-        //search in left half
-        hi= mid-1;
-    }
-}return false;
+        return false;
     }
 }
